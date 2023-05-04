161c49ebd4ddfb54f5a40467c00af887bd856450
Jagannathan CS <jagannathan606@gmail.com>
2023-05-04 14:08:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showAlert() {
+     alert("Our Site is under construction. please come back soon!");
+   }
+
+
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open')
+  nav.classList.toggle('flex')
+  nav.classList.toggle('hidden')
+})
